848a5171a7a22ba5653d25764c102e03d816a083
jmf <jmf@105e78be-af40-0410-829b-9fdcf32e0047>
2008-02-04 09:54:03 +0000

    refactor the recipe and make an update method. start and stop db, isdbstarted, create_bin_scripts, configure_port and do_cmds methods
    git-svn-id: https://subversion.makina-corpus.net/zopina/eggs/makina.recipe.postgres/trunk@311 105e78be-af40-0410-829b-9fdcf32e0047

--- makina/recipe/postgres/__init__.py
+++ makina/recipe/postgres/__init__.py
@@ -20,13 +20,11 @@ import os
 import time
 from random import choice
 
-pg_ctl_script = """
-#!/bin/sh
+pg_ctl_script = """#!/bin/sh
 PGDATA=%s %s/pg_ctl $@
 """
 
-psql_script = """
-#!/bin/sh
+psql_script = """#!/bin/sh
 %s/psql $@
 """
 
@@ -47,105 +45,107 @@ class Recipe(object):
             buildout['buildout']['parts-directory'],
             name)
 
+    def system(self, cmd):
+        code = os.system(cmd)
+        if code:
+            error_occured = True
+            raise RuntimeError('Error running command: %s' % cmd)
+
+    def pgdata_exists(self):
+        return os.path.exists(self.options['pgdata']) 
+
     def install(self):
         """installer"""
-        already_installed = False
-        err_path = os.path.join(self.options['location'], 'errors.log')
-        out_path = os.path.join(self.options['location'], 'cmds.log')
+        self.logger = logging.getLogger(self.name)
+        self.create_bin_scripts()
         if not os.path.exists(self.options['location']):
             os.mkdir(self.options['location'])
-        error_occured = False
-        error = None
-        def system(cmd):
-            code = os.system(cmd)
-            if code:
-                error_occured = True
-                raise RuntimeError('Error running command: %s' % cmd)
-        logger = logging.getLogger(self.name)
-        pgdata = self.options['pgdata']
-        pgdata_exists = os.path.exists(pgdata) 
-            
-        bin = self.options.get('bin','')
-        #TODO: manage path in a better way to handle PATH case
-        initdb = self.options.get('initdb',None)
-        if initdb and not pgdata_exists:
-            system('%s/initdb %s' % (bin, initdb) )
-        
-        port = self.options.get('port',None)
-        if port:
-            logger.warning( " !!!!!!!!!!!! " )
-            logger.warning( " Warning port is not tested at the moment" )
-            logger.warning( " !!!!!!!!!!!! " )
-            # Update the port setting and start up the server
-            #FIXME we need to get pgdata from initdb option
-            conffile = '%s/postgresql.conf' % pgdata
-            f = open(conffile)
-            conf = ('port = %s' % port).join(f.read().split('#port = 5432'))
-            f.close()
-            open(conffile, 'w').write(conf)
-        
-        # Need to know where is the bin directory of buildout
-        buildout_bin_path = self.buildout['buildout']['bin-directory']
-        # Create a wrapper script for psql user and admin
-        script = open('%s/psql'%(buildout_bin_path) , 'w')
-        script.write(psql_script % (bin))
-        script.close()
-        
-        # Create a wrapper script for pg_ctl
-        
-        # Need to knwo where is
-        pg_ctl = '%s/pg_ctl'%(buildout_bin_path)
-        script = open(pg_ctl , 'w')
-        script.write(pg_ctl_script % (pgdata, bin))
-        script.close()
-        
-        #start/restart your engine !
-        PIDFILE = '%s/postmaster.pid' % pgdata
-        if os.path.exists(pg_ctl) and os.path.exists(PIDFILE):
-            logger.info( 'Shutting down PostgreSQL server...' )
-            system('%s stop' % pg_ctl)
-            while os.path.exists(PIDFILE):
-                time.sleep(1)
-        system('%s start'%(pg_ctl))
-        time.sleep(4)
-        
-#        createusers = self.options.get('createusers',None)
-#        if createusers:
-#            createusers = createusers.split(os.linesep)
-#            for user in createusers:
-#                if not user:continue
-#                try: system('%s/createuser %s' % (bin, user) )
-#                except RuntimeError,e:
-#                    f.write("An error has occured while adding user %s %s"%(user,os.linesep))
-#                    f.write('%s'%e)
-#                    f.write(os.linesep)
-#        
-#        createdbs = self.options.get('createdbs', None)
-#        if createdbs:
-#            createdbs = createdbs.split(os.linesep)
-#            for db in createdbs:
-#                if not db:continue
-#                try: system('%s/createdb %s' % (bin, db) )
-#                except RuntimeError,e:
-#                    f.write("An error has occured while adding db %s %s"%(db,os.linesep))
-#                    f.write('%s'%e)
-#                    f.write(os.linesep)
-#        
-        cmds = self.options.get('cmds', None)
-        if cmds:
-            cmds = cmds.split(os.linesep)
-            for cmd in cmds:
-                if not cmd: continue
-                try: system('%s/%s' % (bin, cmd))
-                except RuntimeError, e:
-                    pass
-        dest = self.options['location']
-
-        if error_occured:
-            logger.error('One or more error has occured. Please check log file %s' % (os.path.join(self.options['location'], 'errors.log')))
-            raise RuntimeError , error
-        return dest
+        #Donrt touch an existing database
+        if self.pgdata_exists():
+            self.stopdb()
+            return self.options['location']
+        self.stopdb()
+        self.initdb()
+        self.configure_port()
+        self.startdb()
+        self.do_cmds()
+        self.stopdb()
+        return self.options['location']
 
     def update(self):
         """updater"""
-        pass
+        self.logger = logging.getLogger(self.name)
+        self.create_bin_scripts()
+        if not self.pgdata_exists():
+            self.stopdb()
+            self.initdb()
+            self.startdb()
+            self.do_cmds()
+        self.configure_port()
+        self.stopdb()
+        return self.options['location']
+
+    def startdb(self):
+        if os.path.exists(os.path.join(self.options.get('pgdata'),'postmaster.pid')):
+            self.system('%s restart'%(self.bin_pg_ctl))
+        else:
+            self.system('%s start'%(self.bin_pg_ctl))
+        time.sleep(4)
+
+    def stopdb(self):
+        if os.path.exists(os.path.join(self.options.get('pgdata'),'postmaster.pid')):
+            self.system('%s stop'%(self.bin_pg_ctl))
+            time.sleep(4)
+
+    def isdbstarted(self):
+        PIDFILE = os.path.join(self.options.get('pgdata'),'postmaster.pid')
+        return os.path.exists(pg_ctl) and os.path.exists(PIDFILE)
+
+    def create_bin_scripts(self):
+        buildout_bin_path = self.buildout['buildout']['bin-directory']
+        # Create a wrapper script for psql user and admin
+        psql = os.path.join(buildout_bin_path,'psql')
+        script = open(psql , 'w')
+        script.write(psql_script % (self.options.get('bin')))
+        script.close()
+        os.chmod(psql, 0755)
+        pg_ctl = os.path.join(buildout_bin_path,'pg_ctl')
+        script = open(pg_ctl, 'w')
+        script.write(pg_ctl_script % (self.options.get('pgdata'), self.options.get('bin')))
+        script.close()
+        os.chmod(pg_ctl, 0755)
+        self.bin_pg_ctl = pg_ctl
+        self.bin_psql = psql
+        return pg_ctl, psql
+
+    def initdb(self):
+        initdb_options = self.options.get('initdb',None)
+        bin = self.options.get('bin','')
+        if initdb_options and not self.pgdata_exists():
+            self.system('%s %s' % (os.path.join(bin, 'initdb'), initdb_options) )
+
+    def configure_port(self):
+        port = self.options.get('port',None)
+        if not port: return None
+        self.logger.warning( " !!!!!!!!!!!! " )
+        self.logger.warning( " Warning port is not tested at the moment" )
+        self.logger.warning( " !!!!!!!!!!!! " )
+        # Update the port setting and start up the server
+        #FIXME we need to get pgdata from initdb option
+        conffile = os.path.join(self.options.get('pgdata'),'postgresql.conf')
+        f = open(conffile)
+        conf = ('port = %s' % port).join(f.read().split('#port = 5432'))
+        f.close()
+        open(conffile, 'w').write(conf)
+
+    def do_cmds(self):
+        cmds = self.options.get('cmds', None)
+        bin = self.options.get('bin')
+        if not cmds: return None
+        cmds = cmds.split(os.linesep)
+        for cmd in cmds:
+            if not cmd: continue
+            try: self.system('%s/%s' % (bin, cmd))
+            except RuntimeError, e:
+                pass
+        dest = self.options['location']
